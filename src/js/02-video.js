2bb9a4b57b3031af319fd30cdb8aebe7facdcdac
merry <k.maria.m.24.05@gmail.com>
2023-04-24 15:56:43 +0300

    add three tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// get
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(seconds => {
+        localStorage.setItem('videoplayer-current-time', seconds);
+    });
+}, 1000)
+);
+
+// set and update
+const updateCurrentTime = () => {
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+    if (currentTime) { return player.setCurrentTime(currentTime).then(function (seconds) { }) };
+};
+updateCurrentTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const submit = document.querySelector('form');
+const STORAGE_KEY = 'feedback-form-state';
+
+// add to LS
+const addDataToLS = () => {
+    const dataLS = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+    input.addEventListener('input', throttle((e) => {
+        const email = e.target.value;
+        dataLS.email = email;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(dataLS));
+    },500)
+    );
+    textarea.addEventListener('input', throttle((e) => {
+        const message = e.target.value;
+        dataLS.message = message;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(dataLS));
+    }, 500)
+    );
+};
+addDataToLS();
+
+// reload page
+const onPageReload = () => {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedData) {
+        input.value = savedData.email;
+        textarea.value = savedData.message;
+    };
+};
+onPageReload();
+
+// submit
+submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+});
